a788dff86169515337b028b99d569852b098f214
Lee Jae Hyun <myleje@gmail.com>
2021-07-13 15:32:32 +0900

    .

---
+++ Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,17 @@
+#pragma warning disable IDE0051
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * 800.0f);
+    }
+
+}
