08b1010ecbd5e0b7ac0792f7e682c64d0a1f83f3
Lee Jae Hyun <myleje@gmail.com>
2021-07-13 16:49:19 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
+
     void OnCollisionEnter(Collision coll)
     {
         if (coll.collider.CompareTag("BULLET"))
         {
             Destroy(coll.gameObject);
+            // 충돌지점의 충돌 정보(좌표, 법선벡터, ...)
+            ContactPoint contactPoint = coll.GetContact(0);
+            // 좌표
+            Vector3 pos = contactPoint.point;
+            // 법선벡터를 쿼터니언 타입으로 변환
+            Quaternion rot = Quaternion.LookRotation(-contactPoint.normal);
+
+            GameObject spark = Instantiate(sparkEffect, pos, rot);
+            Destroy(spark, 0.5f);
         }
     }
 }
