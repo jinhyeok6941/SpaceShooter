4d88ca0ce35c0b853e7f9f5237d462e0ff9afbbd
Lee Jae Hyun <myleje@gmail.com>
2021-07-14 11:49:49 +0900

    .

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+/*
+    유한상태머신 (Finite State Machine) FSM
+    NPC AI
+
+*/
