9593efef3e6e31705950a62e0f43a516dc1df50e
Lee Jae Hyun <myleje@gmail.com>
2021-07-14 16:44:00 +0900

    .

---
+++ Assets/02.Scripts/MyGizmos.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    public Color _color = Color.green;
+    public float _radius = 0.3f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = _color;
+        Gizmos.DrawSphere(transform.position, _radius);
+    }
+}
