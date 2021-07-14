c0d224083a9d337f43d4ea9c6f4510a1d82d6441
Lee Jae Hyun <myleje@gmail.com>
2021-07-14 11:18:08 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -42,9 +42,18 @@ public class FireCtrl : MonoBehaviour
     // 코루틴 (Co-Routine)
     IEnumerator ShowMuzzleFlash()
     {
+        // Texture Offset
         // x,y = 0.0f, 0.5f / (0, 1) * 0.5f => (0.0f, 0.5f)
         Vector2 offset = new Vector2(Random.Range(0, 2), Random.Range(0, 2)) * 0.5f;
         muzzleFlash.material.mainTextureOffset = offset;
+
+        // Rotate 
+        float angle = Random.Range(0, 360);
+        muzzleFlash.transform.localRotation = Quaternion.Euler(Vector3.forward * angle);//Quaternion.Euler(0, 0, angle)
+
+        // Scale
+        float scale = Random.Range(1.0f, 3.0f);
+        muzzleFlash.transform.localScale = Vector3.one * scale; // new Vector3(scale, 1, scale)
 
         // MuzzleFlash의 MeshRenderer 활성화
         muzzleFlash.enabled = true;
