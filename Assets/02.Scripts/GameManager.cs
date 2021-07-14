ea09c6bc1b517f8c840bcb3e4ded8ac8eade2e16
Lee Jae Hyun <myleje@gmail.com>
2021-07-14 17:04:22 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<Transform> points = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>(true, points);
     }
 
     // Update is called once per frame
